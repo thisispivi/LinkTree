dc5b5bd56aa86a574e7ad535d2ab25770a28586b
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2024-06-12 20:21:47 +0200

    feat: create page

--- linktree/src/main.tsx
+++ linktree/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/_global.scss";
+import "./styles/_typography.scss";
+import "./styles/_variables.scss";
+import "./styles/_mixins.scss";
+import "./styles/_scrollbar.scss";
+import "./i18n/i18n";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider
+      router={createHashRouter([
+        {
+          path: "/",
+          element: <div />,
+          children: [{ path: "japan", element: <div /> }],
+        },
+      ])}
+    />
+  </React.StrictMode>
+);
---
+++ linktree/src/pages/Japan/Japan.tsx
@@ -0,0 +1,3 @@
+export default function Japan() {
+  return <div className="japan">JAPAN</div>;
+}
